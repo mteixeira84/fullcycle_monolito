d1e745fb8e7fe06799af42a2c0a25361163b6bb9
Matheus Teixeira <matheus.analista@gmail.com>
2023-11-17 10:03:25 -0300

    feat: Implements payment

---
+++ src/modules/client-adm/usecase/find-client/find-client.usecase.dto.ts
@@ -0,0 +1,12 @@
+export interface FindClientUseCaseInputDto {
+  id: string;
+}
+
+export interface FindClientUseCaseOutputDto {
+  id: string;
+  name: string;
+  email: string;
+  address: string;
+  createdAt: Date;
+  updatedAt: Date;
+}
---
+++ src/modules/client-adm/usecase/find-client/find-client.usecase.ts
@@ -0,0 +1,28 @@
+import ClientGateway from "../../gateway/client.gateway";
+import {
+  FindClientUseCaseInputDto,
+  FindClientUseCaseOutputDto,
+} from "./find-client.usecase.dto";
+
+export default class FindClientUseCase {
+  private _clientRepository: ClientGateway;
+
+  constructor(clientRepository: ClientGateway) {
+    this._clientRepository = clientRepository;
+  }
+
+  async execute(
+    input: FindClientUseCaseInputDto
+  ): Promise<FindClientUseCaseOutputDto> {
+    const client = await this._clientRepository.find(input.id);
+
+    return {
+      id: client.id.id,
+      name: client.name,
+      email: client.email,
+      address: client.address,
+      createdAt: client.createdAt,
+      updatedAt: client.updatedAt,
+    };
+  }
+}
